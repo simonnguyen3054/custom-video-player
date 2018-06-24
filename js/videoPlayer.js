3c64876bd0fcdaebdab6308f273ae1c53027f37a
Simon Nguyen <simonnguyen3054@gmail.com>
2018-06-24 10:18:32 -0700

    show and hide controls

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,4 +1,5 @@
 (function(window, document){
+  //declare variables to referenced elements
   var video = document.getElementsByTagName('video')[0],
       videoControls = document.getElementById('videoControls'),
       play = document.getElementById('play'),
@@ -12,7 +13,36 @@
   //Create videoPlayer object to cover all the methods
   var videoPlayer = {
     init: function() {
+      //set var that to the object videoPlayer
+      var that = this;
+      //Trigger css when javascript is enabled
+      document.documentElement.className = 'js';
+      //Get rid of default controls
+      video.removeAttribute('controls');
+      //When meta data is ready, show the controls
+      video.addEventListener('loadeddata', this.initializeControls, false);
+    },
+    initializeControls: function() {
+      //When all meta data has loaded, show the controls
+      videoPlayer.showHideControls();
+    },
+    showHideControls: function() {
+      //shows and hides the video player
+      video.addEventListener('mouseover', function() {
+        videoControls.style.opacity = 1;
+      }, false);
 
+      videoControls.addEventListener('mouseover', function() {
+        videoControls.style.opacity = 1;
+      }, false);
+
+      video.addEventListener('mouseout', function() {
+        videoControls.style.opacity = 0;
+      }, false);
+
+      videoControls.addEventListener('mouseout', function() {
+        videoControls.style.opacity = 0;
+      })
     }
   };
 
